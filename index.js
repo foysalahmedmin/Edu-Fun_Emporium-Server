7f52b77b7208724714902bbce642b50dfed021ad
FOYSAL AHMED MiN <foysalahmedmin@gmail.com>
2023-05-19 00:25:18 +0600

    maked some fake data by chatgpt and inserted in database. And testing get request functionality

--- index.js
+++ index.js
@@ -21,13 +21,18 @@ const client = new MongoClient(uri, {
 });
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
     client.connect();
-    // Send a ping to confirm a successful connection
+    const database = client.db("Edu_Fun_Emporium_DB");
+    const EduToysCollection = database.collection("Edu_Toys_Collection");
+
+    app.get('/toys', async(req, res) => {
+      const result = await EduToysCollection.find().toArray()
+      res.send(result)
+    })
+
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
-    // Ensures that the client will close when you finish/error
     // await client.close();
   }
 }
